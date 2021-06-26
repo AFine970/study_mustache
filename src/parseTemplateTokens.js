e6d771d5e08a752f4628b7a1701f50cf16717e22
cunhang_wei <cunhang_wei@kingdee.com>
2021-06-26 15:40:28 +0800

    feat: 完成一维的token解析

---
+++ src/parseTemplateTokens.js
@@ -0,0 +1,30 @@
+import Scanner from './Scanner'
+
+/**
+ * 将模板字符串转换为tokens
+ * @param {string} templateStr 
+ */
+export default function parseTemplateToTokens(templateStr) {
+    let tokens = []
+    const scanner = new Scanner(templateStr)
+
+    let words
+    while(!scanner.eos()) {
+        words = scanner.scanUntil('{{')
+        if (words) {
+            tokens.push(['text', words])
+        }
+        scanner.scan('{{')
+        
+        words = scanner.scanUntil('}}')
+        if (words) {
+            if (words[0] === '#') {
+                tokens.push(['#', words.substring(1)])
+            } else {
+                tokens.push(['name', words])
+            }
+        }
+        scanner.scan('}}')
+    }
+    return tokens
+}
